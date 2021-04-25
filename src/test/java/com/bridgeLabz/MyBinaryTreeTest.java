b54cefbb3e16848e9f4db914227d95d9bc331e33
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-25 21:26:36 +0530

    [Added] Creating BST and added 56,30,70

---
+++ src/test/java/com/bridgeLabz/MyBinaryTreeTest.java
@@ -0,0 +1,18 @@
+package com.bridgeLabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyBinaryTreeTest {
+    //UC1 Ability to create BST by adding 56, 30 and 70
+    @Test
+    public void given3NumbersWhenAddedToBSTShouldReturnSize3(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(13, size);
+    }
+}
